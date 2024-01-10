c337a20d4f92bd947ea2a1b6b4e2e09f5ffe217a
Kota Takahashi <59428418+Grampus86@users.noreply.github.com>
2024-01-10 21:17:58 +0900

    kadai_011を追加

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,21 @@
+list = ["水", "金", "地", "火", "木", "土", "天", "海", "冥"]
+
+"""
+forを使ったループ
+"""
+
+for element in list:
+    print(element)
+
+"""
+whileを使ったループ
+"""
+
+num = 0
+# リストの要素数の数を取得
+list_length = len(list)
+# num=0から1ループ毎にnumを1ずつ加算
+# リストをインデックスが(リストの要素数の数-1)以下ならループ
+while num <= list_length - 1:
+    print(list[num])
+    num += 1
