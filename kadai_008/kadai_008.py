c3f9e112a19ae3e69967b274b673d49fa888cc50
Kota Takahashi <59428418+Grampus86@users.noreply.github.com>
2024-01-10 20:38:08 +0900

    条件分岐の判定を修正

--- kadai_008/kadai_008.py
+++ kadai_008/kadai_008.py
@@ -3,7 +3,7 @@ var = 6  # 3の倍数
 # var = 30 # 3の倍数と5の倍数の両方に該当
 # var = 11 # 上記のどの場合にも該当しない
 
-if (var % 15 == 0) and (var % 5 == 0):
+if (var % 3 == 0) and (var % 5 == 0):
     print('FizzBuzz')
 elif var % 3 == 0:
     print('Fizz')
